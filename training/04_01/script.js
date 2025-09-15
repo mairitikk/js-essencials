49dee5ce00d0aa47db897f1fa265477f2b66cfed
mairitikk <mairi.tikk@gmail.com>
2025-09-15 11:08:40 +0200

    body changes

--- training/04_01/script.js
+++ training/04_01/script.js
@@ -15,6 +15,21 @@ const everydayPack = new Backpack(
   false,
   "December 5, 2018 15:00:00 PST"
 );
+const content = `
+ <main>
+      <article>
+        <h1>Everyday Backpack</h1>
+        <ul>
+          <li>Volume:</li>
+          <li>Color:</li>
+          <li>Age:</li>
+          <li>Number of pockets:</li>
+          <li>Left strap length:</li>
+          <li>Right strap length:</li>
+          <li>Lid status:</li>
+        </ul>
+      </article>
+    </main>`;
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
